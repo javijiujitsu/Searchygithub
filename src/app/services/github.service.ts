e1cd72d165c9336282390418d488e6621455db82
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-26 14:17:24 -0500

    added github Api

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,2 +1,13 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+  private username: string;
+  private client_id = 'Iv1.22c0350e5196dd19';
+  private client_secret = '049abc137d217269208b0addc04d0e0ecac9c374';
+
+}
+  
