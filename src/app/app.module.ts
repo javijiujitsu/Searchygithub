62058b752cc5117d620dd60c75830c981d1dcf5a
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-26 19:32:42 -0500

    fixed profile, correct commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TheProfileComponent } from './the-profile/the-profile.component';
@@ -12,6 +13,7 @@ import { TheProfileComponent } from './the-profile/the-profile.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     AlertModule.forRoot()
   ],
   providers: [],
