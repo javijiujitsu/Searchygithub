596d5c5a66b093b123b2e18f385e1c528b17da9a
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-26 19:30:53 -0500

    added http module and completed service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -9,5 +9,13 @@ export class GithubService {
   private client_id = 'Iv1.22c0350e5196dd19';
   private client_secret = '049abc137d217269208b0addc04d0e0ecac9c374';
 
+  constructor(private _http: Http) {
+     console.log('Github Service Ready');
+     this.username = 'javijiujitsu';
+  }
+  getUser(){
+      return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+      .map(res => res.json());
+  }
+
 }
-  
