849fbe4372addf24c403a0be5bceeab3167a745f
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-26 19:49:38 -0500

    added github service to profile component and service is ready and working

--- src/app/the-profile/the-profile.component.ts
+++ src/app/the-profile/the-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../services/github.service';
 
 @Component({
   selector: 'app-the-profile',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TheProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _githubService: GithubService) { 
+
+  }
 
   ngOnInit() {
   }
