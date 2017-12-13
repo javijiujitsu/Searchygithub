7fc3cda88db0b5d9da91550f0357ed1791673660
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-12 23:21:36 -0500

    imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 import {HttpModule} from '@angular/http';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TheProfileComponent } from './the-profile/the-profile.component';
 
@@ -14,7 +14,8 @@ import { TheProfileComponent } from './the-profile/the-profile.component';
   imports: [
     BrowserModule,
     HttpModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
